aadb28aa5299f8a72da820b5f1fc5f200efeaee2
Aamershwayat <amerabdalhameed77@gmail.com>
2022-01-25 10:44:37 +0200

    task05

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+var yourName = prompt("What is your name ?")
+console.log ("his/her name is :" , yourName  )
+ 
+var gender = confirm("If you are male press: ok, if you are female press: cancle ")
+console.log ("the gender" , gender)
+
+if (gender==true){
+    gender= "male"
+}else { gender =  "female"}
+console.log ("the gender" , gender)
+
+var age = prompt ("How old are you?")
+if (age <=0)
+{age = prompt ("How old are you, please venter a valid age?")}
+console.log ("age", age) ;
+confirm(`welcom $ {yourName}/n press yes to move the wecoming message`);
+
+var nickName = "Mr" ;
+var errorMsg = "" ; 
+gender.tolowercase() === "male" ?
+nickName :
+( nickName = "Mr")  ;
+
+gender.tolowercase() === "female" ?
+nickName = "Mrs" :
+(erorrMsg = "you entered answer wrong ")
+
+erorrMsg != "again" ?
+alert (`welcom ${yourName}`) :
+alert (`welcom again ${nickName} ${yourName}`) ;
