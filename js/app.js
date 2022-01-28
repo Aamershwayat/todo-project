b5248004577d55e4953383680bb33d264b933eb5
Aamershwayat <amerabdalhameed77@gmail.com>
2022-01-28 04:51:10 +0200

    task06

--- js/app.js
+++ js/app.js
@@ -1,33 +1,58 @@
 'use strict'
 
+function getUserName() {
+  var name = prompt("What is your name ?")
+  console.log("his/her name is :", name)
+  if (name == "") {
+    name = "invalid"
+  }
+  return name;
+}
 
-var yourName = prompt("What is your name ?")
-console.log ("his/her name is :" , yourName  )
- 
-var gender = confirm("If you are male press: ok, if you are female press: cancle ")
-console.log ("the gender" , gender)
 
-if (gender==true){
-    gender= "male"
-}else { gender =  "female"}
-console.log ("the gender" , gender)
+function getUserGender() {
+  var gender = confirm("If you are male press: ok, if you are female press: cancel ")
+  console.log("the gender", gender)
+  if (gender == true) {
+    gender = "male"
+  } else { gender = "female" }
 
-var age = prompt ("How old are you?")
-if (age <=0)
-{age = prompt ("How old are you, please venter a valid age?")}
-console.log ("age", age) ;
-confirm(`welcom $ {yourName}/n press yes to move the wecoming message`);
+  console.log("the gender", gender)
+  if (gender == "") {
+    gender = "invalid"
+  }
+  return gender;
+}
 
-var nickName = "Mr" ;
-var errorMsg = "" ; 
-gender.toLowerCase() === "male" ?
-nickName :
-( nickName = "Mr")  ;
+function getAge() {
+  var age = prompt("How old are you?")
+  if (age <= 0) { age = prompt("How old are you, please enter a valid age?") }
+  console.log("age", age);
+  confirm(`welcome ${getUserName()} \nPress yes to move the welcoming message`);
+  if (age == "") {
+    age = "invalid"
+  };
+  return age;
+}
 
-gender.toLowerCase() === "female" ?
-nickName = "Mrs" :
-errorMsg = "you entered answer wrong "
+function getNickName() {
+  var nickName = "Mr";
+  var errorMsg = "";
+  gender.toLowerCase() === "male" ? nickName : (nickName = "Mr");
+  gender.toLowerCase() === "female" ? nickName = "Mrs" :errorMsg = "you entered answer wrong ";
+  if (nickName == "") {
+    nickName = "invalid"
+  }
+  errorMsg != "again" ? alert(`welcome ${getUserName()}`) : alert(`welcome again ${getNickName()} ${getUserName()}`);
+  return nickName;
+}
+const array = [getUserName(), getUserGender(), getAge(), getNickName(), married, children, disability];
+var married = confirm("Are You Married?");
+var children = confirm("Do You Have Any Children?");
+var disability = confirm("Do You Have Any Disability?");
+console.log(array);
 
-errorMsg != "again" ?
-alert (`welcom ${yourName}`) :
-alert (`welcom again ${nickName} ${yourName}`) ;
+for (let i = 0; i < array.length; i++) {
+  console.log("userAnswer", array[i])
+}
+
